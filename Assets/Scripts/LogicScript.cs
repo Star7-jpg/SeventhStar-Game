ee61747ae4339ac26173b426cfa54aa462d01ead
Star7-jpg <marcosconde612@gmail.com>
2025-11-02 18:36:43 -0600

    Agregada validación para la función de conteo de puntos

--- Assets/Scripts/LogicScript.cs
+++ Assets/Scripts/LogicScript.cs
@@ -7,9 +7,9 @@ public class LogicScript : MonoBehaviour
     public Text scoreText; //Referencia al componente Text que muestra la puntuación
 
     [ContextMenu("Incrementa la puntuación")] //Permite llamar a la función desde el menú contextual del componente en el editor
-    public void addScore() 
+    public void addScore( int scoreToAdd ) 
     {
-        playerScore = playerScore + 1; 
+        playerScore = playerScore + scoreToAdd; //Incrementa la puntuación del jugador
         scoreText.text = playerScore.ToString(); //Actualiza el texto de la puntuación en pantalla
     }
 }
